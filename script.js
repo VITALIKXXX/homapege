487f115492799527719a9da7b8f8a05a373ffef2
Vitalii Tomenko <vitalikxxx985@gmail.com>
2023-02-20 14:32:41 +0100

    create functions

--- script.js
+++ script.js
@@ -2,26 +2,27 @@
     const welcome = () => {
         console.log("Czesc!");
     }
-    
+
     const toggleBackgroun = () => {
+        const element = document.querySelector(".js-element");
+        const themeName = document.querySelector(".js-element__themeName");
         element.classList.toggle("color");
-    
+
         if (element.classList.contains("color")) {
             themeName.innerText = "jasniejszy";
         } else {
             themeName.innerText = " ciemniejszy";
-    
+
         }
-    
+
     };
-    welcome();
-    
-    
-    const przycisk = document.querySelector(".js-element__przycisk");
-    const element = document.querySelector(".js-element");
-    const themeName = document.querySelector(".js-element__themeName");
-    
-    przycisk.addEventListener("click", toggleBackgroun);
-    
-    
+
+    const init = () => {
+
+        const przycisk = document.querySelector(".js-element__przycisk");
+        przycisk.addEventListener("click", toggleBackgroun);
+
+        welcome();
+    }
+    init();
 }
